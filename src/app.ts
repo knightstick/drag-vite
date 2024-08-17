1a205ed49517ab8b22efe38f6860586e8a38f9a0
Chris Jewell <chrisjohnjewell@gmail.com>
2024-08-17 06:41:09 +0000

    Use sortable for drag and drop

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
+import Sortable from "sortablejs";
+
 const greeting: string = "hello, world!";
 
-function renderColumn(numberOfBoxes: number, container: HTMLElement) {
+function renderColumn(
+  numberOfBoxes: number,
+  index: number,
+  container: HTMLElement
+) {
   const column = document.createElement("div");
-  column.className = "flex flex-col w-full p-4";
+  column.className = "flex-1 flex-col h-full p-4";
 
   for (let i = 0; i < numberOfBoxes; i++) {
     const div = document.createElement("div");
     div.className =
       "bg-blue-200 border border-blue-500 flex-1 p-4 mb-2 cursor move text-center";
-    div.textContent = greeting;
+    div.textContent = `Column ${index} - Div ${i + 1}`;
 
     column.appendChild(div);
   }
+
+  var sortable = Sortable.create(column, {
+    group: "shared",
+    animation: 20,
+  });
 
   container.appendChild(column);
 }
@@ -22,7 +33,11 @@ function render() {
     return;
   }
 
-  renderColumn(3, container);
+  const columnNumbers: number[] = [5, 3, 0, 1];
+
+  columnNumbers.forEach((numberOfBoxes, index) => {
+    renderColumn(numberOfBoxes, index, container);
+  });
 }
 
 render();
